de2015826417c609368e7e51a31db74c9577a12e
dawright22 <53329137+dawright22@users.noreply.github.com>
2021-01-27 20:04:15 +1100

    Add files via upload

---
+++ Cluster_AKS/varables.tf
@@ -0,0 +1,15 @@
+variable "cluster-name" {
+}
+
+variable "appId" {
+  default = "05419517-31a2-48e0-bb47-89924dc8c0d3"
+}
+
+variable "password" {
+  default = "wm-6c1Gg4SxY8tmLB59mQbZss1d84_.mvW"
+}
+
+variable "location" {
+  default = "East US"
+
+}
