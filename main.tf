12ee797543393b7939a57eec133ac68a7ef06f38
dawright22 <53329137+dawright22@users.noreply.github.com>
2021-01-28 10:48:58 +1100

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,16 +11,22 @@ resource "random_pet" "name" {
 module "Cluster_EKS" {
   source       = "./Cluster_EKS"
   cluster-name = random_pet.name.id
+  aws_region  = "ap-southeast-2"
 }
 
 #MSFT
 module "Cluster_AKS" {
   source       = "./Cluster_AKS"
   cluster-name = random_pet.name.id
+  appId        = "05419517-31a2-48e0-bb47-89924dc8c0d3"
+  password     =  "wm-6c1Gg4SxY8tmLB59mQbZss1d84_.mvW"
 }
 
 #Google
 module "Cluster_GKE" {
   source       = "./Cluster_GKE"
   cluster_name = random_pet.name.id
+  gcp_region   = "australia-southeast1"
+  gcp_zone     = "australia-southeast1-c"
+  gcp_project  = "dwright-gcp-demos"
 }
