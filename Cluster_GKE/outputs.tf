b5459627dfe0e7290fed2cf18eb9ad6ba93b45aa
dawright22 <53329137+dawright22@users.noreply.github.com>
2021-01-28 11:08:28 +1100

    Update outputs.tf

--- Cluster_GKE/outputs.tf
+++ Cluster_GKE/outputs.tf
@@ -23,5 +23,6 @@ output "k8s_master_auth_cluster_ca_certificate" {
 // Auth to k8s cluster 
 output "gcloud_connect_command" {
   value = "gcloud container clusters get-credentials ${var.cluster_name} --zone ${var.gcp_zone} --project ${var.gcp_project}"
+  sensitive   = true
 }
 
