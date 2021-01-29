4147e71ba61ebac629f84d626493edf8eb24fff0
dawright22 <53329137+dawright22@users.noreply.github.com>
2021-01-29 16:02:35 +1100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,16 +1,16 @@
 variable "gcp_region" {
   description = "GCP region, e.g. us-east1"
-  default     = "australia-southeast1"
+  default     = ""
 }
 
 variable "gcp_zone" {
   description = "GCP zone, e.g. us-east1-b (which must be in gcp_region)"
-  default     = "australia-southeast1-c"
+  default     = ""
 }
 
 variable "gcp_project" {
   description = "GCP project name"
-  default     = "dwright-gcp-demos"
+  default     = ""
 }
 variable "aws_region" {
   default     = ""
